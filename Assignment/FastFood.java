99e52383c2d9bf8621f897f3059cbb6da05f7b82
Kent-Herrera <kentherrera85@gmail.com>
2026-02-03 21:12:13 +0800

    Update customer name and order ID in FastFood

--- Assignment/FastFood.java
+++ Assignment/FastFood.java
@@ -1,7 +1,7 @@
 public class FastFood {
     public static void main(String[] args) {
-        Customer customer = new Customer("Jane Smith");
-        Order order = new Order(101, 12.50);
+        Customer customer = new Customer("Leng Ganda");
+        Order order = new Order(67, 12.50);
         customer.placeOrder(order);
 
         System.out.println(customer.getFullDetails());
